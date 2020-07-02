83161104f63c530767984b438b7a860af009de09
Rahul Raj <mogilirahulraj@gmail.com>
2020-07-02 12:16:37 -0500

    the ids and url works but not element_texts

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
+const fetch = require('node-fetch')
 const {
   GraphQLSchema,
   GraphQLObjectType,
@@ -8,52 +9,42 @@ const {
   GraphQLInt,
   GraphQLNonNull
 } = require('graphql')
+const { json, text } = require('express')
 const app = express()
 
-const authors = [
-	{ id: 1, name: 'J. K. Rowling' },
-	{ id: 2, name: 'J. R. R. Tolkien' },
-	{ id: 3, name: 'Brent Weeks' }
-]
+const BASE_URL = 'https://sites.lib.uh.edu/kmneuma2/api/items?page=2';
 
-const books = [
-	{ id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
-	{ id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
-	{ id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
-	{ id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
-	{ id: 5, name: 'The Two Towers', authorId: 2 },
-	{ id: 6, name: 'The Return of the King', authorId: 2 },
-	{ id: 7, name: 'The Way of Shadows', authorId: 3 },
-	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
-]
+function fetchPersonByURL(url) {
+  return fetch(url).then(res => res.json())
+}
 
-const BookType = new GraphQLObjectType({
-  name: 'Book',
-  description: 'This represents a book written by an author',
+// function fetchCoinDetailsByURL(url) {
+//   // return fetch(url).then(res => res.json())
+//   const response = await fetch('https://sites.lib.uh.edu/kmneuma2/api/items?page=2');
+//   resp = response.json();
+//   return resp.element_texts;
+// }
+
+// function fetchElementTextsByUrl(url) {
+//   return fetch(url).then(res => json())
+// }
+
+const CoinType = new GraphQLObjectType({
+  name: 'Coin',
+  description: 'These are details about the coin',
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLInt) },
-    name: { type: GraphQLNonNull(GraphQLString) },
-    authorId: { type: GraphQLNonNull(GraphQLInt) },
-    author: {
-      type: AuthorType,
-      resolve: (book) => {
-        return authors.find(author => author.id === book.authorId)
-      }
-    }
+    url: { type: GraphQLNonNull(GraphQLString) },
   })
 })
 
-const AuthorType = new GraphQLObjectType({
-  name: 'Author',
-  description: 'This represents a author of a book',
+const ElementTextsType = new GraphQLObjectType({
+  name: 'Element Details',
+  description: 'Represents the details of the coin',
   fields: () => ({
-    id: { type: GraphQLNonNull(GraphQLInt) },
-    name: { type: GraphQLNonNull(GraphQLString) },
-    books: {
-      type: new GraphQLList(BookType),
-      resolve: (author) => {
-        return books.filter(book => book.authorId === author.id)
-      }
+    element_texts: { 
+      type: GraphQLNonNull(ElementTextsType),
+      resolve: ids => ids.element_texts.map(fetchPersonByURL)
     }
   })
 })
@@ -62,70 +53,34 @@ const RootQueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'Root Query',
   fields: () => ({
-    book: {
-      type: BookType,
-      description: 'A Single Book',
+    ids: {
+      type: new GraphQLList(CoinType),
+      description: 'List of all ids',
       args: {
-        id: { type: GraphQLInt }
+        id: { type: GraphQLString }
       },
-      resolve: (parent, args) => books.find(book => book.id === args.id)
+      resolve: () => fetchPersonByURL(BASE_URL)
     },
-    books: {
-      type: new GraphQLList(BookType),
-      description: 'List of All Books',
-      resolve: () => books
-    },
-    authors: {
-      type: new GraphQLList(AuthorType),
-      description: 'List of All Authors',
-      resolve: () => authors
-    },
-    author: {
-      type: AuthorType,
-      description: 'A Single Author',
+    url: {
+      type: new GraphQLList(CoinType),
+      description: 'List of all url',
       args: {
-        id: { type: GraphQLInt }
+        url: { type: GraphQLString }
       },
-      resolve: (parent, args) => authors.find(author => author.id === args.id)
-    }
-  })
-})
-
-const RootMutationType = new GraphQLObjectType({
-  name: 'Mutation',
-  description: 'Root Mutation',
-  fields: () => ({
-    addBook: {
-      type: BookType,
-      description: 'Add a book',
-      args: {
-        name: { type: GraphQLNonNull(GraphQLString) },
-        authorId: { type: GraphQLNonNull(GraphQLInt) }
-      },
-      resolve: (parent, args) => {
-        const book = { id: books.length + 1, name: args.name, authorId: args.authorId }
-        books.push(book)
-        return book
-      }
+      resolve: () => fetchPersonByURL(BASE_URL)
     },
-    addAuthor: {
-      type: AuthorType,
-      description: 'Add an author',
-      args: {
-        name: { type: GraphQLNonNull(GraphQLString) }
-      },
-      resolve: (parent, args) => {
-        const author = { id: authors.length + 1, name: args.name }
-        authors.push(author)
-        return author
-      }
-    }
+    // element_texts: {
+    //   type: new GraphQLList(ElementTextsType),
+    //   description: 'Details of the coin',
+    //   args: {
+    //     text: { type: GraphQLString }
+    //   },
+    // }
   })
 })
 
 const schema = new GraphQLSchema({
-  query: RootQueryType,
-  mutation: RootMutationType
+  query: RootQueryType
 })
 
 app.use('/graphql', expressGraphQL({
